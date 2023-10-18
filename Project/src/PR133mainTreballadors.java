4d826622f0b23badccc31065bbe88910306b9b93
Roberto Zambrano <rzajr868@gmail.com>
2023-10-18 08:15:19 +0200

    .

---
+++ Project/src/PR133mainTreballadors.java
@@ -0,0 +1,11 @@
+public class PR133mainTreballadors {
+    
+    public static void main(String[] args) {
+        editarvalor("nom");
+
+    }
+
+    public static void editarvalor(String nombreatributo){
+        
+    }
+}
